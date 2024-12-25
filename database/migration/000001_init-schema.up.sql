53cffd6e2fca64b6c76d30b24c091f22c55aefd1
augustus <augustus@carem.io>
2024-12-25 16:44:41 +0700

    [database]: notification table

--- database/migration/000001_init-schema.up.sql
+++ database/migration/000001_init-schema.up.sql
@@ -1,7 +1,8 @@
+CREATE TYPE notification_type AS ENUM ('email', 'discord', 'telegram');
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   email VARCHAR UNIQUE NOT NULL,
-  fullname VARCHAR UNIQUE NOT NULL,
   hashed_password TEXT NOT NULL,
   created_at TIMESTAMP DEFAULT NOW(),
   updated_at TIMESTAMP DEFAULT NOW()
@@ -28,4 +29,21 @@ CREATE TABLE assets (
   updated_at TIMESTAMP DEFAULT NOW()
 );
 
+CREATE TABLE notifications (
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(id) ON DELETE CASCADE,
+  noti_type notification_type NOT NULL DEFAULT 'email', 
+  message TEXT,
+  is_read BOOLEAN DEFAULT FALSE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE user_followed_assets (
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(id) ON DELETE CASCADE,
+  asset_id INT REFERENCES assets(id) ON DELETE CASCADE,
+  followed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE INDEX ON "assets" ("slug");
+CREATE INDEX ON "assets" ("cmc_id");
--- internal/repository/user/user_repository.go
+++ internal/repository/user/user_repository.go
@@ -2,10 +2,15 @@ package repository
 
 import (
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+
+	db "github.com/augustus281/trackingcoin/database/sqlc"
+	"github.com/augustus281/trackingcoin/global"
 )
 
 type IUserRepository interface {
-	GetByEmail(ctx *gin.Context, email string)
+	Create(ctx *gin.Context, email, hashedPassword string) (db.User, error)
+	GetByEmail(ctx *gin.Context, email string) (db.User, error)
 }
 
 type userRepo struct{}
@@ -14,5 +19,18 @@ func NewUserRepo() IUserRepository {
 	return &userRepo{}
 }
 
-func (r *userRepo) GetByEmail(ctx *gin.Context, email string) {}
- 
+func (r *userRepo) GetByEmail(ctx *gin.Context, email string) (db.User, error) {
+	user, err := global.Db.Queries.GetUserByEmail(ctx, email)
+	if err != nil {
+		global.Logger.Error("not found user by email ",
+			zap.String("email", email),
+			zap.Error(err),
+		)
+		return db.User{}, err
+	}
+	return user, nil
+}
+
+func (r *userRepo) Create(ctx *gin.Context, email, hashedPassword string) (db.User, error) {
+	return db.User{}, nil
+}
